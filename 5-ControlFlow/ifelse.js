1f05aa153c91a66585accd47120a965a9a80b701
Rahul <160539002+rahul0049@users.noreply.github.com>
2025-11-25 02:25:40 +0530

    Add examples of if-else control flow
    Add commented examples of if-else statements and conditions.

---
+++ 5-ControlFlow/ifelse.js
@@ -0,0 +1,35 @@
+//if statement 
+// if(condition){
+
+// }
+// else{
+
+// }  === also check typeof 
+
+
+// const  temperature=500
+// if(temperature<50){
+//     console.log("less than 50")
+// }
+// else {
+//     console.log("Not less than 50")
+
+// }
+
+
+
+// const score=200
+// if(score>100){
+//     const power="Fly"
+//     console.log(`User Power: ${power}`)
+
+// }
+// else{
+//     console.log(`User Power${power}`) // gives error as there is no power in scope
+// }
+
+
+
+// if(userLoggedIn && debitCard && temp) {}  and 
+// else if(dsfa || kjsd){}                   or
+// else {}
